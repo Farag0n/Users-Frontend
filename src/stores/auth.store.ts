b729f948a989dd9b7f38ffd952fd4a96c5896180
faragon <miguelanga1604@gmail.com>
2026-01-14 06:39:46 -0500

    feat: explain coments

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,1 +1,5 @@
-import { defineStore } from 'pinia'
+//la carpeta stores sirve pra almacenar datos globales
+//accesibles para cualquier componente
+//tambien ayuda a separar la logica de negocio de los componentes
+//y facilita la reutilizacion, varios componentes pueden usar el mismo store
+import { defineStore } from 'pinia';
