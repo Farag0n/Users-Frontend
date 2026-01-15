f3b97a464b1fc16fc473e3a478ca3d6b9e54298e
faragon <miguelanga1604@gmail.com>
2026-01-15 06:45:23 -0500

    Update: DTOs to User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,36 +1,12 @@
-//los objetos son un reflejo de los DTOs de mi backend
+import type { UserRole } from "./enums/UserRole";
 
-// Lo que recibimos del Backend al loguearnos
-export interface AuthResponse {
-    accessToken: string;
-    refreshToken: string;
-    message: string;
-}
-
-// Lo que enviamos para Login
-export interface LoginDto {
-    email: string;
-    password: string;
-}
-
-// Lo que enviamos para Registro (Debe coincidir con UserRegisterDto del backend)
-export interface RegisterDto {
+export interface UserResponseDto {
+    id: string;
     name: string;
     lastName: string;
     email: string;
     userName: string;
     password: string;
-    confirmPassword: string;
-    role: number; // 0 = Admin, 1 = User
-}
-
-// Usuario decodificado (opcional por ahora, para mostrar info en pantalla)
-export interface User {
-    email: string;
-    token: string;
-}
-
-export enum UserRole {
-    Admin,
-    User
-}
+    userRole: UserRole;
+    isDeleted: boolean;
+};
