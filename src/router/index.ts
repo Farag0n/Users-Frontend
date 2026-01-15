7eee2e49e0e7a875329ca5821ca513ea4613fd35
faragon <miguelanga1604@gmail.com>
2026-01-15 07:07:24 -0500

    Update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";// se importa el vue-router
 //se importan las vistas, el @ se usa como un alias que apunta a la carpeta src
-import LoginView from "@/views/Login.vue"; 
+import LoginView from "@/views/LoginView.vue"; 
 import RegisterView from "@/views/Register.vue"
 
 //si hace un array de objetos con las diferentes rutas
+//el path es la url del navegador y el component es la vista que renderiza esa ruta
+//el name es para navegacion por name asi si se cambia el path no se tinen que refactorizar todo
 const routes = [
-    {path: "/login", component: LoginView},//el path es la url del navegador y el component es la vista que renderiza esa ruta
-    {path: "/register", component: RegisterView}
+    {
+        path: "/login",
+        name: "login",
+        component: LoginView
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: RegisterView
+    },
+    {
+        path: "/home",
+        name: "home",
+        component: Home
+    }
 ];
 
 //ejemplo de ruta dinamica
